1f0dcb6e27f12d333ceafac8c77b929c6471a867
Dilip Solanki <dilipsolanki1020@gmail.com>
2023-08-20 23:09:08 +0530

    manage order complete

--- src/app/components/orders/manage-order/manage-order.component.ts
+++ src/app/components/orders/manage-order/manage-order.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { OrderDataService ,Order} from 'src/app/services/order-data.service';
+// import { Order } from './order.model';
 @Component({
   selector: 'app-manage-order',
   templateUrl: './manage-order.component.html',
   styleUrls: ['./manage-order.component.css']
 })
-export class ManageOrderComponent {
+export class ManageOrderComponent implements OnInit {
+  orders!: Order[];
+  selectedOrder: Order | null = null;
+  isEditMode: boolean = false;
+  constructor(private orderService : OrderDataService){}
+ngOnInit(): void {
+  this.orders = this.orderService.getOrders();
+}
+
+showOrderDetails(order: Order) {
+  this.selectedOrder = order;
+}
+
+clearSelectedOrder() {
+  this.selectedOrder = null;
+}
+
+toggleEditMode() {
+  this.isEditMode = !this.isEditMode;
+}
+
+saveOrderDetails() {
+
+  if (this.selectedOrder) {
+    this.orderService.updateOrder(this.selectedOrder);
+  }
+  this.toggleEditMode();
+}
+
+deleteOrder() {
+  if (this.selectedOrder) {
+    const confirmation = confirm('Are you sure you want to delete this order?');
+    if (confirmation) {
+      this.orderService.deleteOrder(this.selectedOrder);
+      this.selectedOrder = null;
+    }
+  }
+}
 
 }
--- src/app/services/order-data.service.ts
+++ src/app/services/order-data.service.ts
@@ -4,6 +4,72 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class OrderDataService {
+  public orders: Order[] = [
+    {
+      orderId: 1,
+      senderName: 'John Doe',
+      senderAddress: '123 Main St',
+      senderContactNumber: '555-123-4567',
+      receiverName: 'Jane Smith',
+      receiverAddress: '456 Elm St',
+      receiverContactNumber: '555-987-6543',
+      city: 'Sampleville',
+      title: 'Sample Item',
+      weight: '5 lbs',
+      dimensions: '10x8x6 inches',
+      quantity: 2
+    },
+    {
+      orderId: 2,
+      senderName: 'Alice Johnson',
+      senderAddress: '789 Oak St',
+      senderContactNumber: '555-555-5555',
+      receiverName: 'Bob Brown',
+      receiverAddress: '101 Pine St',
+      receiverContactNumber: '555-111-2222',
+      city: 'Testburg',
+      title: 'Test Product',
+      weight: '2.5 lbs',
+      dimensions: '6x6x6 inches',
+      quantity: 1
+    }
+    // Add more sample orders here if needed
+  ];
 
   constructor() { }
+
+
+  addOrder(order: Order) {
+    this.orders.push(order);
+  }
+
+  getOrders() {
+    return this.orders;
+  }
+
+  updateOrder(updatedOrder: Order) {
+    const index = this.orders.findIndex(order => order.orderId === updatedOrder.orderId);
+    if (index !== -1) {
+      this.orders[index] = updatedOrder;
+    }
+  }
+
+  deleteOrder(orderToDelete: Order) {
+    this.orders = this.orders.filter(order => order.orderId !== orderToDelete.orderId);
+  }
+}
+
+export interface Order {
+  orderId: number;
+  senderName: string;
+  senderAddress: string;
+  senderContactNumber: string;
+  receiverName: string;
+  receiverAddress: string;
+  receiverContactNumber: string;
+  city: string;
+  title: string;
+  weight: string;
+  dimensions: string;
+  quantity: number;
 }
